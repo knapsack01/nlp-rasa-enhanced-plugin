06c784ba7972d7d1b6dbeaa0286d9dd9a85cd260
knapsack01 <113893531+knapsack01@users.noreply.github.com>
2023-03-26 20:25:36 +0000

    Add missing imports.

--- src/main/java/co/aurasphere/botmill/rasa/exception/RasaPluginError.java
+++ src/main/java/co/aurasphere/botmill/rasa/exception/RasaPluginError.java
@@ -20,3 +20,96 @@
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package co.aurasphere.botmill.rasa.exception;
+
+import java.io.Serializable;
+import com.google.gson.annotations.SerializedName;
+
+
+/**
+ * Object that represents an error from Kik.
+ * 
+ * @author Alvin P. Reyes
+ */
+public class RasaPluginError implements Serializable {
+
+	/**
+	 * The serial version UID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The error message.
+	 */
+	private String message;
+
+	/**
+	 * The error type.
+	 */
+	private String type;
+
+	/**
+	 * The error code.
+	 */
+	private String code;
+
+	/**
+	 * The error Facebook's trace ID.
+	 */
+	@SerializedName("fbtrace_id")
+	private String fbTraceId;
+
+	/**
+	 * Gets the {@link #message}.
+	 *
+	 * @return the {@link #message}.
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * Sets the {@link #message}.
+	 *
+	 * @param message
+	 *            the {@link #message} to set.
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * Gets the {@link #type}.
+	 *
+	 * @return the {@link #type}.
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * Sets the {@link #type}.
+	 *
+	 * @param type
+	 *            the {@link #type} to set.
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * Gets the {@link #code}.
+	 *
+	 * @return the {@link #code}.
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * Sets the {@link #code}.
+	 *
