a876fcee828f1d5ff537a58a8a17bf9697d5ab06
knapsack01 <113893531+knapsack01@users.noreply.github.com>
2023-03-26 19:03:21 +0000

    Remove unneeded comments from role defaults

--- src/main/java/co/aurasphere/botmill/rasa/exception/RasaPluginError.java
+++ src/main/java/co/aurasphere/botmill/rasa/exception/RasaPluginError.java
@@ -1,3 +1,4 @@
+
 /**
  * 
  * MIT License
@@ -19,38 +20,3 @@
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- * 
- */
-package co.aurasphere.botmill.rasa.builder;
-
-
-/**
- * The Class QueryBuilder.
- */
-public class QueryBuilder {
-	
-	/** The query. */
-	private String query;
-
-	/**
-	 * Gets the query.
-	 *
-	 * @return the query
-	 */
-	public String getQuery() {
-		return query;
-	}
-
-	/**
-	 * Sets the query.
-	 *
-	 * @param query the new query
-	 */
-	public void setQuery(String query) {
-		this.query = query;
-	}
-	
-	
-}
