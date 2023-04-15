73b4d485b473f52ff42cfcaa9457439a683aaf74
knapsack01 <113893531+knapsack01@users.noreply.github.com>
2023-04-15 05:34:39 +0000

    Update auth.php

--- src/main/java/co/aurasphere/botmill/rasa/incoming/rasa/model/DucklingTimeEntityValue.java
+++ src/main/java/co/aurasphere/botmill/rasa/incoming/rasa/model/DucklingTimeEntityValue.java
@@ -1,1 +1,5 @@
-package co.aurasphere.botmill.rasa.incomin
+package co.aurasphere.botmill.rasa.incoming.rasa.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class DucklingTimeEntityValue extends Ra
