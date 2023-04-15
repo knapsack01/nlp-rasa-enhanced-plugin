f211a8a3b45bd92e552ef4dfd319405f15d365c8
knapsack01 <113893531+knapsack01@users.noreply.github.com>
2023-04-15 13:21:02 +0000

    profiling

--- src/main/java/co/aurasphere/botmill/rasa/incoming/rasa/model/DucklingTimeEntityValue.java
+++ src/main/java/co/aurasphere/botmill/rasa/incoming/rasa/model/DucklingTimeEntityValue.java
@@ -2,4 +2,9 @@ package co.aurasphere.botmill.rasa.incoming.rasa.model;
 
 import com.google.gson.annotations.SerializedName;
 
-public class DucklingTimeEntityValue extends Ra
+public class DucklingTimeEntityValue extends RasaEntityValue{
+	
+	@SerializedName("from")
+	private String from;
+	@SerializedName("to")
+	private 
