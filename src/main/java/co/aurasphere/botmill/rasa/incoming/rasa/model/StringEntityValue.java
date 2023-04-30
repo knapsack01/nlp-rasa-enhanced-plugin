28bd78af95aa1e99cbcab154c8b345b308cd3fc5
knapsack01 <113893531+knapsack01@users.noreply.github.com>
2023-04-30 14:25:20 +0000

    clear cache

--- src/main/java/co/aurasphere/botmill/rasa/incoming/rasa/model/StringEntityValue.java
+++ src/main/java/co/aurasphere/botmill/rasa/incoming/rasa/model/StringEntityValue.java
@@ -2,4 +2,9 @@ package co.aurasphere.botmill.rasa.incoming.rasa.model;
 
 import com.google.gson.annotations.SerializedName;
 
-public class S
+public class StringEntityValue extends RasaEntityValue{
+	
+	@SerializedName("value")
+	private String stringValue;
+
+	pu
