06731d5f80c64b2daaf193c1e5c01d5f7ba4d86b
knapsack01 <113893531+knapsack01@users.noreply.github.com>
2023-05-31 17:23:25 +0000

    updated sponsors

--- src/main/java/co/aurasphere/botmill/util/EnumLowercaseSerializer.java
+++ src/main/java/co/aurasphere/botmill/util/EnumLowercaseSerializer.java
@@ -2,4 +2,7 @@
  * 
  * MIT License
  *
- * Copyright (c) 2017 Bot
+ * Copyright (c) 2017 BotMill.io
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this 
