6ca35d6c5f25e5635b97f5f927f5a4b5977cf586
knapsack01 <113893531+knapsack01@users.noreply.github.com>
2023-06-09 18:15:14 +0000

    Ignore dist/

--- src/main/java/co/aurasphere/botmill/util/NetworkConstants.java
+++ src/main/java/co/aurasphere/botmill/util/NetworkConstants.java
@@ -5,4 +5,5 @@
  * Copyright (c) 2017 BotMill.io
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction,
