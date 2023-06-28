3256276026f2c6ddc520669cd7a8e2c4314fec3c
knapsack01 <113893531+knapsack01@users.noreply.github.com>
2023-06-28 21:40:37 +0000

    1.1.8

--- src/main/java/co/aurasphere/botmill/util/NetworkUtils.java
+++ src/main/java/co/aurasphere/botmill/util/NetworkUtils.java
@@ -34,4 +34,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 
-import 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods
